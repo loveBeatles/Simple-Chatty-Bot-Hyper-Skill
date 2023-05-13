47eacf709c79b73246a99af6807a53f23ae687f1
Vanya <118664638+loveBeatles@users.noreply.github.com>
2023-05-13 10:48:26 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
 def bot_introduce(name, age):
     return f"Hello my name is {name}.\nI was created in {age}."
+
+def user_name():
+    print("Please, remind me your name.")
+    _name = input()
+    return f"What a great name you have, {_name}!"
+
 print(bot_introduce("OniGiri", 2023))
+print(user_name())
