eb4b6037e4a18adc63a20b6fd061b4dcf91af341
Vanya <118664638+loveBeatles@users.noreply.github.com>
2023-05-13 11:11:56 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,5 +6,11 @@ def user_name():
     _name = input()
     return f"What a great name you have, {_name}!"
 
+def guess_age(rem3, rem5, rem7):
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    return f"Your age is {age}; that's a good time to start programming!"
+
 print(bot_introduce("OniGiri", 2023))
 print(user_name())
+print("Let me guess your age.\nEnter reminders of dividing your age by 3, 5 and 7")
+print(guess_age(int(input()), int(input()), int(input())))
